6405f094f254e81be3bc9ce45cc76813253442d1
Desperados <89983547+Desperados-Creation@users.noreply.github.com>
2021-09-24 20:21:21 +0200

    Update server.lua

--- dpr_Karting/server.lua
+++ dpr_Karting/server.lua
@@ -9,20 +9,16 @@ end)
 
 RegisterNetEvent('dpr_Karting:SpawnKart')
 AddEventHandler('dpr_Karting:SpawnKart', function(Label, Value, Price)
-    local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
+    local _src = source
+	local xPlayer = ESX.GetPlayerFromId(_src)
     local car = Value
 	
 	if(xPlayer.getBank() >= Price) then
 		xPlayer.removeAccountMoney('bank', Price)
         TriggerClientEvent('dpr_Karting:spawnCar',source,car)
-        local xPlayers	= ESX.GetPlayers()
-		for i=1, #xPlayers, 1 do
-			local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-			Citizen.Wait(10000)
-			TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Banque', 'Conseiller', 'Un prélèvement de ~r~'..Price..'$ ~s~a été effectué sur votre compte pour une location de karting !', 'CHAR_BANK_MAZE', 1)
-		end
+		Citizen.Wait(10000)
+		TriggerClientEvent('esx:showAdvancedNotification', _src, 'Banque', 'Conseiller', 'Un prélèvement de ~r~'..Price..'$ ~s~a été effectué sur votre compte pour une location de karting !', 'CHAR_BANK_MAZE', 1)
 	else
-		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Banque', 'Conseiller', 'Vous n\'avez pas suffisament d\'argent sur votre compte bancaire!', 'CHAR_BANK_MAZE', 2)
+		TriggerClientEvent('esx:showAdvancedNotification', _src, 'Banque', 'Conseiller', 'Vous n\'avez pas suffisament d\'argent sur votre compte bancaire!', 'CHAR_BANK_MAZE', 2)
 	end	
 end)
